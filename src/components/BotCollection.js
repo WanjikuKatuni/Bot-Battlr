73e374c127b72feb695d5dd56d518bcc90b9e03e
wanjiku <annritawanjiku77@gmail.com>
2022-10-03 09:37:25 +0300

    all bots displayed

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,21 @@
 import React from "react";
+import BotCard from "./BotCard";
+// import BotsPage from "./BotsPage";
 
-function BotCollection() {
+function BotCollection(botInfo, botArmy,setBotArmy, setBotInfo) {
   // Your code here
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
         Collection of all bots
+        {botInfo.map((bot)=> (
+          <BotCard key={bot.id}
+          bot={bot}
+          botArmy={botArmy}
+          setBotArmy={setBotArmy}
+          setBotInfo={setBotInfo} />
+        ))}
       </div>
     </div>
   );
